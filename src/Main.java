b5774fdae886e6acf1c1ef1bc5e6fe323be9a593
zxcv109999 <55624041+zxcv109999@users.noreply.github.com>
2019-10-27 07:25:50 +0800

    M2Q11

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        Scanner scn =new Scanner(System.in);
         int n = scn.nextInt();
-        double sum = 0;
-        for (int i= 1;i<=n;i++){
-            sum = sum+(double) 1/(double) ((2*i-1)*(2*i));
-        }System.out.println(sum);
+        for (int i =1;i<=n;i++){
+            if(n%i==0){
+                System.out.println(i);}
+        }
     }
 }
