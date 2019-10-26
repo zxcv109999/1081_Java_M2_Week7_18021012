5f8fe420a12d7ea5e9fe8fcaed247b108ac178fa
zxcv109999 <55624041+zxcv109999@users.noreply.github.com>
2019-10-27 07:27:13 +0800

    M2Q12

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn =new Scanner(System.in);
         int n = scn.nextInt();
+        String c = scn.next();
         for (int i =1;i<=n;i++){
-            if(n%i==0){
-                System.out.println(i);}
+            for (int j = 1;j<=n;j++){
+                System.out.print(c);
+            }System.out.println();
         }
+
     }
 }
+
