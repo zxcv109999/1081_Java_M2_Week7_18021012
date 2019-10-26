0a56134bf7c1927e0e0b217f29d854cfcc6df770
zxcv109999 <55624041+zxcv109999@users.noreply.github.com>
2019-10-27 07:24:48 +0800

    M2Q15

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        long sum = 1;
-        for(int i = 1;i<=n;i++){
-            sum = sum*i;
+        double sum = 0;
+        for (int i= 1;i<=n;i++){
+            sum = sum+(double) 1/(double) ((2*i-1)*(2*i));
         }System.out.println(sum);
     }
 }
