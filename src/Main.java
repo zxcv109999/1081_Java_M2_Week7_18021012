97e4c18e44a4e602d08d950a77e5eb9f5f00c7c0
zxcv109999 <55624041+zxcv109999@users.noreply.github.com>
2019-10-28 21:25:00 +0800

    M2Q8

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn =new Scanner(System.in);
-        int n = scn.nextInt();
-        int sum=1;
-        int t=0;
-        for (int i = 1;i<=n;i++){
-            sum*=2;
-            t=t+sum;
-        }
-        System.out.println(t);
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int sum = 0;
+        for(int i=3;i<=a;i+=3){
+            sum = sum+i;
+
+
+        } System.out.println(sum);
     }
 }
-
