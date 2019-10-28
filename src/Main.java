fbda0d07169b6a4925f7b31bd3aade49a7fda0c6
zxcv109999 <55624041+zxcv109999@users.noreply.github.com>
2019-10-28 21:37:39 +0800

    M2Q10

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int sum=0;
-        int k=0;
-        boolean bln=true;
-        for (int i = 2;i<=n;i++){
-            k=i;
-            for (;k!=1;){
-                if (k%2==0){
-                    k=k/2;
-                }
-                else if (k%3==0){
-                    k=k/3;
-                }
-                else if (k%5==0){
-                    k=k/5;
-                }
-                else {
-                    bln=false;
-                    break;
-                }
-            }
-            if (bln){
-                sum=sum+i;
-            }
-            bln=true;
+        for(int i = 1;i<=n;i++){
+            for (int j = 1;j<=n;j++){
+                System.out.print(i*j+"\t");
+            }System.out.println();
         }
-        System.out.println(sum);
     }
 }
