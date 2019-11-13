a2daa0c1ead67c9955ed913c1b57ef4354ff9c5a
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 16:01:07 +0800

    M2Q26

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
             Scanner scn = new Scanner(System.in);
-            long n =scn.nextLong();
-            while (n>0){
-                System.out.print(n%10);
-                n/=10;
+            int n=scn.nextInt();
+            int k=n;
+            while (n>=3){
+                k+=n/3;
+                n=n/3+n%3;
             }
-            System.out.println();
-
+            System.out.println(k);
         }
 }
