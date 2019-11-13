dbe5f627b8c155e2470774b1fd2df8efe670171e
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 15:52:38 +0800

    M2Q23

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,12 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
             Scanner scn = new Scanner(System.in);
-            int n=scn.nextInt();
-            for (int i =3;i<n;i++){
-                for (int j=i+1;j<n;j++){
-                    for (int k =j+1;k<n;k++){
-                        if (i+j+k<=n){
-                            if (i*i+j*j==k*k){
-                                System.out.print(i+"\t");
-                                System.out.print(j+"\t");
-                                System.out.print(k+"\t");
-                            }
-                        }
-                    }
-                }
+            long n =scn.nextLong();
+            while (n>0){
+                System.out.print(n%10);
+                n/=10;
             }
+            System.out.println();
+
         }
 }
